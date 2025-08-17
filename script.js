0efc90f2c31b65bf14a271f170801c1a6adeee40
Boricaweb <thanapolsrangkaew@gmail.com>
2025-08-17 16:28:35 +0700

    2nd commit finished the general tasks

--- script.js
+++ script.js
@@ -8,6 +8,11 @@ function generateTarget() {
     return Math.floor(Math.random() * 10);
 }
 
+/*Check the distance between the human guess to target, and computer guess to target
+const getAbsoluteDistance = (humanDistance, computerDistance) => {
+    if ()
+}*/
+
 // Compare the human guess and computer guess against the target number
 const compareGuesses = (humanGuess, computerGuess, target) => {
     if (Math.abs(humanGuess - target) <= Math.abs(computerGuess - target)) {
@@ -17,9 +22,14 @@ const compareGuesses = (humanGuess, computerGuess, target) => {
     }
 }
 
+// Update the score based on the winner
 const updateScore = (winner) => {
     switch (winner) {
         case 'human': humanScore++; break;
         case 'computer': computerScore++; break;   
     }
+}
+//Advance to the next round
+const advanceRound = () => {
+    currentRoundNumber++;
 }
